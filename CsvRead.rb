da39fda62ddeecd84d78905be426694c8cb803e1
yowasou <jun.makita@gmail.com>
2013-07-27 13:59:09 +0900

    second

---
+++ CsvRead.rb
@@ -0,0 +1,8 @@
+# encoding: utf-8
+require 'csv'
+
+class CsvRead
+	def initialize(file)
+		
+	end
+end
---
+++ CsvRead_spec.rb
@@ -0,0 +1,11 @@
+# encoding: utf-8
+require './CsvRead.rb'
+
+describe CsvRead do
+  filename = "店舗.csv"
+  csvread = CsvRead.new
+  context "readed" do
+    #subject { stablemarriage.match(male,female) }
+    #it {should == [["1","a"],["2","c"],["3","b"],["4","d"]]}
+  end    
+end
